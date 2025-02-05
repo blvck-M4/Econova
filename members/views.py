d74b66a616c14122157f2b2259ca481a60075bd3
Baskeil <gabrielngomariendeau@gmail.com>
2025-02-05 14:44:29 -0500

    Home Page

--- members/views.py
+++ members/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('home.html')
     print("View Loaded")
 
     return HttpResponse(template.render())
