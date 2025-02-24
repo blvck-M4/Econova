0be09674cc785da52bedf985993c02e5b2cffc90
Baskeil <62712616+Baskeil@users.noreply.github.com>
2025-02-24 16:29:06 -0500

    Page de connexion et d'inscription (Base de données)

--- members/models.py
+++ members/models.py
@@ -1,3 +1,15 @@
+from datetime import timezone
+
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    prenom = models.CharField(max_length=100, null=True)
+    nom_de_famille = models.CharField(max_length=100, null=True)
+    utilisateur = models.CharField(max_length=100, null=True)
+    email = models.EmailField()
+    mot_de_passe = models.CharField(max_length=100)
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom_de_famille}"
