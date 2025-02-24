b7c2824ae19e630c19c5103336432c1303801f2f
Baskeil <62712616+Baskeil@users.noreply.github.com>
2025-02-24 09:10:00 -0500

    Page de connexion et section outils de home html/css

--- members/views.py
+++ members/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.views.decorators.csrf import csrf_exempt
 def members(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
-
 def connexion(request):
+    if request.method == 'POST':
+        print(request.POST)
+
     template = loader.get_template('connexion.html')
-    return HttpResponse(template.render())
+    return render(request, 'connexion.html')
