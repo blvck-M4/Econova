7b745f48a511ce3b160a55ba7e19bdfce7cf625a
Baskeil <62712616+Baskeil@users.noreply.github.com>
2025-02-26 16:00:53 -0500

    html et css -Gabriel

--- members/urls.py
+++ members/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
-    path('members/connexion', views.connexion, name='connexion'),
-    path('members/rejoindre', views.rejoindre, name='rejoindre'),
+    path('', views.members, name='members'),
+    path('connexion', views.connexion, name='connexion'),
+    path('rejoindre', views.rejoindre, name='rejoindre'),
 
-    path('members/deconnexion', views.deconnexion, name='deconnexion'),
+    path('deconnexion', views.deconnexion, name='deconnexion'),
 
 ]
